018608f3387b30d59c3f91121b6bd6faf72e2a33
Alex Johnson <alex.johnson3141@gmail.com>
2017-12-13 12:13:02 -0600

    started day 10 and finished day 7

--- day7/day7.py
+++ day7/day7.py
@@ -24,7 +24,7 @@ input_file.close()
 
 
 # Find bottom node (part 1)
-base_node = node_parents.keys()[0]
+base_node = list(node_parents.keys())[0]
 
 while base_node in node_parents.keys():
     base_node = node_parents[base_node]
@@ -42,6 +42,59 @@ def get_weight(node):
             total_weight += get_weight(key)
     return total_weight
 
+def get_children(node):
+    global node_parents
+    children = []
+    for key in node_parents.keys():
+        if node_parents[key] == node:
+            children.append(key)
+    return children
 
-def find_unbalanced(node):
-    if node in node_parents.keys():
+def get_unbalanced(node):
+    """Return the node that needs to be rebalanced."""
+    global node_weights
+    print("balancing " + node)
+    children = get_children(node)
+    weights = {}
+    base_weights = {}
+    weight_frequency = {}
+    base_weight_frequency = {}
+    for child in children:
+        weight = get_weight(child)
+        weights[child] = weight
+        base_weights[child] = node_weights[child]
+        if weight in weight_frequency.keys():
+            weight_frequency[weight] += 1
+        else:
+            weight_frequency[weight] = 1
+        if base_weights[child] in base_weight_frequency.keys():
+            base_weight_frequency[base_weights[child]] += 1
+        else:
+            base_weight_frequency[base_weights[child]] = 1
+    if len(base_weight_frequency.keys()) > 1:
+        print("ONE OF THE DIRECT CHILDREN OF "+node+" IS UNBALANCED")
+    if len(weight_frequency.keys()) > 1:
+        # find unbalanced child
+        min_weight_freq = min(weight_frequency.values())
+        problem_child = None
+        for child in children:
+            if weight_frequency[weights[child]] == min_weight_freq:
+                problem_child = child
+                break
+        print("problem child is " + problem_child)
+        return get_unbalanced(problem_child)
+    else:
+        print("CHILDREN OF NODE " + node + " ARE BALANCED")
+        return node
+
+rebalance_node = get_unbalanced(base_node)
+print("Weight of problem child: " + str(get_weight(rebalance_node)))
+siblings = get_children(node_parents[rebalance_node])
+if siblings[0] == rebalance_node:
+    siblings = siblings[1:]
+weight_goal = get_weight(siblings[0])
+weight_children = 0
+children = get_children(rebalance_node)
+for child in children:
+    weight_children += get_weight(child)
+print(weight_goal - weight_children)
