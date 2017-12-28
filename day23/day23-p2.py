17b07d3793c51572dbe116a867ab4a0a491a0775
Alex Johnson <alex.johnson3141@gmail.com>
2017-12-28 13:33:07 -0600

    day 23 pt 2 initial decomp

--- day23/day23-p2.py
+++ day23/day23-p2.py
@@ -3,3 +3,38 @@
 # Day 23
 
 # TODO decompile input.txt
+b = 65
+c = 65
+b *= 100
+b += 100000
+c = b
+c += 17000
+h = 0
+
+while True:
+    f = 1
+    d = 2
+    while True:
+        e = 2
+        while True:
+            g = d
+            g *= e
+            g -= b
+            if g == 0:
+                f = 0
+            e += 1
+            g = e
+            g -= b
+        g += 8
+        d += 1
+        g = d
+        g -= b
+    if f == 0:
+        h += 1
+    g = b
+    g -= c
+    if g == 0:
+        break
+    b += 17
+
+print(h)
